96b445f505e2450989610b26f1ebef0b6a0970aa
brendon <brendondammm@hotmail.com>
2023-07-19 22:15:35 +1000

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import Canvas from './canvas';
+import Customiser from './pages/Customiser';
+import Home from './pages/Home';
+function App() {
+
+  return (
+    <main className='app transition-all ease-in'>
+      <Home />
+      <Canvas />
+      <Customiser />
+    </main>
+  )
+}
+
+export default App
